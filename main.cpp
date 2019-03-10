d3a6275f2d183c23a7f5ff6cbe71ccf633fc220a
lostapplesauce <48301604+lostapplesauce@users.noreply.github.com>
2019-03-09 16:54:07 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,118 @@
+echo "# blue-parakeet" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/mk1159/blue-parakeet.git
+git push -u origin master
+#include <iostream>
+using namespace std;
+
+bool isWon(char, char[][3]);
+bool isDraw(char[][3]);
+void displayBoard(char[][3]);
+void makeAMove(char[][3], char);
+
+int main() {
+    //
+    //    PLEASE DO NOT CHANGE function main
+    //
+    char board[3][3] = { { ' ', ' ', ' ' },{ ' ', ' ', ' ' },{ ' ', ' ', ' ' } };
+    displayBoard(board);
+    
+    while (true) {
+        // The first player makes a move
+        makeAMove(board, 'X');
+        displayBoard(board);
+        if (isWon('X', board)) {
+            cout << "X player won" << endl;
+            exit(0);
+        }
+        else if (isDraw(board)) {
+            cout << "No winner" << endl;
+            exit(0);
+        }
+        
+        // The second player makes a move
+        makeAMove(board, 'O');
+        displayBoard(board);
+        
+        if (isWon('O', board)) {
+            cout << "O player won" << endl;
+            exit(0);
+        }
+        else if (isDraw(board)) {
+            cout << "No winner" << endl;
+            exit(0);
+        }
+    }
+    return 0;
+}
+bool isWon(char a, char board[][3]){
+    for (int i = 0; i < 3; i++){
+        if(board[i][0]== a && board[i][1] == a && board[i][2]==a)
+            return true;
+        if(board[0][i]== a && board[1][i] == a && board[2][i]==a)
+            return true;
+    }
+    if(board[0][0] == a && board[1][1] == a && board[2][2]==a)
+        return true;
+    if(board[0][2] == a && board[1][1] == a && board[2][0]==a)
+        return true;
+    return false;
+}
+
+bool isDraw(char board[][3]){
+    
+    int count = 0;
+    for(int i = 0; i < 3;i++){
+        for(int j = 0; j < 3; j++){
+            if(board[i][j] == 'X' || board[i][j] == 'O')
+                count++;
+        }
+    }
+    if(count != 9)
+        return false;
+    if(!isWon('X',board) && !isWon('O',board))
+        return true;
+    return 9;
+}
+
+void displayBoard(char board[][3]){
+    
+    cout << "-------------\n";
+    for (int i = 0; i<3; i++){
+        cout << "|";
+        for (int j = 0; j<3; j++){
+            cout << board[i][j] << "|";
+        }
+        
+        cout << endl;
+        cout << "-------------\n";
+        
+    }
+}
+
+
+void makeAMove(char board[][3], char a){
+    
+    while(true){
+        cout << "Enter a row(0,1,2) for player " << a << ":";
+        int b;
+        cin >> b;
+        int c;
+        cout << "Enter a column(0,1,2) for player " << a << ":";
+        cin >> c;
+        if ( b < 0 || b > 2 || c < 0 || c > 2){
+            cout << "Invalid Move\n";
+        }
+        else {
+            if (board[b][c] != 'X' && board[b][c] != 'O'){
+                board[b][c] = a;
+                break;
+            }
+            else {
+                cout << "Invalid Move\n";
+            }
+        }
+    }
+}
